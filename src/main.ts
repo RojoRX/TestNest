86a9122abca2a696df8dc66e65b85426d05943ea
Usuario2 <email@gmail.com>
2023-04-11 01:41:56 -0400

    Ready

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config(); // Carga las variables de entorno
+
+  const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 3000; // Obtiene el puerto de las variables de entorno, o usa el valor por defecto 3000 si no está definido
+  await app.listen(port);
+}
+bootstrap();
