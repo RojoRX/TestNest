9e8defb37bb52e7616f7be1c36d3cc0c854d02b9
Usuario2 <email@gmail.com>
2023-04-11 01:56:51 -0400

    Ready IV

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config(); // Carga las variables de entorno
-
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000; // Obtiene el puerto de las variables de entorno, o usa el valor por defecto 3000 si no está definido
-  await app.listen(port);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
